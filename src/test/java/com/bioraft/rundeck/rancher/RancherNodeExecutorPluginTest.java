6c0b9dfbef06fed7df6e99e8ffe8f25baaaff5c4
Karl DeBisschop <karl.debisschop@bioraft.com>
2020-03-11 23:35:10 -0400

    use logger

---
+++ src/test/java/com/bioraft/rundeck/rancher/RancherNodeExecutorPluginTest.java
@@ -0,0 +1,103 @@
+package com.bioraft.rundeck.rancher;
+
+import com.dtolabs.rundeck.core.common.INodeEntry;
+import com.dtolabs.rundeck.core.execution.ExecutionContext;
+import com.dtolabs.rundeck.core.execution.service.NodeExecutorResult;
+import com.dtolabs.rundeck.core.execution.workflow.steps.StepFailureReason;
+import com.dtolabs.rundeck.core.storage.ResourceMeta;
+import com.dtolabs.rundeck.core.storage.StorageTree;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.rundeck.storage.api.Resource;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.bioraft.rundeck.rancher.Constants.CONFIG_ACCESSKEY_PATH;
+import static com.bioraft.rundeck.rancher.Constants.CONFIG_SECRETKEY_PATH;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RancherNodeExecutorPluginTest {
+
+    @Mock
+    ExecutionContext executionContext;
+
+    @Mock
+    StorageTree storageTree;
+
+    @Mock
+    Resource<ResourceMeta> resource;
+
+    @Mock
+    ResourceMeta resourceMeta;
+
+    @Mock
+    INodeEntry node;
+
+    Map<String, String> nodeAttributes;
+
+    @Before
+    public void setUp() {
+        nodeAttributes = new HashMap<>();
+    }
+
+    @Test
+    public void testDescription() {
+        RancherNodeExecutorPlugin nodeExecutorPlugin = new RancherNodeExecutorPlugin();
+        assertTrue(nodeExecutorPlugin.getDescription().getDescription().startsWith("Executes a command "));
+    }
+
+    @Test
+    public void serviceIsNotYetSupported() {
+        RancherNodeExecutorPlugin nodeExecutorPlugin = new RancherNodeExecutorPlugin();
+        String[] command = { "ls" };
+        nodeAttributes.put("type", "service");
+        when(node.getAttributes()).thenReturn(nodeAttributes);
+        NodeExecutorResult result = nodeExecutorPlugin.executeCommand(executionContext, command, node);
+        String message = "Node executor is not currently supported for services";
+        assertEquals(message, result.getFailureMessage());
+        assertEquals(StepFailureReason.PluginFailed, result.getFailureReason());
+        assertEquals(-1, result.getResultCode());
+    }
+
+    @Test
+    public void missingKeyCreatesFailure() {
+        RancherNodeExecutorPlugin nodeExecutorPlugin = new RancherNodeExecutorPlugin();
+        String[] command = { "ls" };
+        nodeAttributes.put("type", "container");
+        when(node.getAttributes()).thenReturn(nodeAttributes);
+        NodeExecutorResult result = nodeExecutorPlugin.executeCommand(executionContext, command, node);
+        String message = "Storage path is not defined.";
+        assertEquals(message, result.getFailureMessage());
+        assertEquals(StepFailureReason.IOFailure, result.getFailureReason());
+        assertEquals(-1, result.getResultCode());
+    }
+
+    @Test
+    public void missingKeyValueCreatesFailure() throws IOException {
+        RancherNodeExecutorPlugin nodeExecutorPlugin = new RancherNodeExecutorPlugin();
+        String[] command = { "ls" };
+        nodeAttributes.put("type", "container");
+        nodeAttributes.put(CONFIG_ACCESSKEY_PATH, "access_key");
+        nodeAttributes.put(CONFIG_SECRETKEY_PATH, "secret_key");
+        when(node.getAttributes()).thenReturn(nodeAttributes);
+        when(executionContext.getStorageTree()).thenReturn(storageTree);
+        when(storageTree.getResource(anyString())).thenReturn(resource);
+        when(resource.getContents()).thenReturn(resourceMeta);
+        doThrow(new IOException("Storage failure.")).when(resourceMeta).writeContent(any());
+        NodeExecutorResult result = nodeExecutorPlugin.executeCommand(executionContext, command, node);
+        String message = "Storage failure.";
+        assertEquals(message, result.getFailureMessage());
+        assertEquals(StepFailureReason.IOFailure, result.getFailureReason());
+        assertEquals(-1, result.getResultCode());
+    }
+}
