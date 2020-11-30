9b8d7f55aa99705e5df73c9cfe49f85322a10f2e
Karl DeBisschop <kdebisschop@gmail.com>
2020-11-30 17:21:37 -0500

    More logging work

--- src/main/java/com/bioraft/rundeck/rancher/RancherWebSocketListener.java
+++ src/main/java/com/bioraft/rundeck/rancher/RancherWebSocketListener.java
@@ -123,7 +123,7 @@ public class RancherWebSocketListener extends WebSocketListener {
 	private static String[] remoteCommand(String[] command, String temp) {
 		String file = temp + ".pid; ";
 		// Prefix STDERR lines with STDERR_TOKEN to decode in logging step.
-		String cmd = "( " + String.join(" ", command) + "\" 3>&1 1>&2 2>&3 ) ";
+		String cmd = "( " + String.join(" ", command) + " ) 3>&1 1>&2 2>&3 3>&- ";
 		String job = cmd + "| while IFS= read -r line || [ -n \"$line\" ]; do echo " + STDERR_TOKEN + "$line ; done";
 		return new String[]{ "sh", "-c", "printf $$ >>" + file + job + ";printf ' %s' $? >>" + file };
 	}
